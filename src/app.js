4b07fcdd3e5dbac190fa11e6fb47c3cb599c3d0a
Nguyen Duc Khoa <demonhunter942000@gmail.com>
2021-07-15 22:55:06 +0700

    update

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+require("./db/mongoose");
+const userRouter = require("./router/user");
+const taskRouter = require("./router/task");
+const multer = require("multer"); // thư viện middleware cho việc upload file
+
+const app = express();
+
+// nếu viết như này là sẽ chạy khi chạy route bất kì
+// app.use((req,res,next)=>{
+//   res.status(503).send("Site is currently down for maintenance. Check back soon")
+// })
+
+app.use(express.json()); // tự động chuyển đổi incomming JSON data ra Object
+
+app.use(userRouter);
+app.use(taskRouter);
+
+// Finish CRUD(create,read,update,delete) REST API
+
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,16 @@
-const express = require("express");
-require("./db/mongoose");
-const userRouter = require("./router/user");
-const taskRouter = require("./router/task");
-const multer = require("multer")// thư viện middleware cho việc upload file
-
-const app = express();
+const app = require('./app')
 
 //process.env lưu các enviroment variable được set up bởi dev hoặc service
 // các environment variable sẽ được lưu vào các file .env
 const port = process.env.PORT;
 
-
-// nếu viết như này là sẽ chạy khi chạy route bất kì 
-// app.use((req,res,next)=>{
-//   res.status(503).send("Site is currently down for maintenance. Check back soon")
-// })
-
-app.use(express.json()); // tự động chuyển đổi incomming JSON data ra Object
-
-app.use(userRouter);
-app.use(taskRouter);
-
-// Finish CRUD(create,read,update,delete) REST API
-
+// run app server
 app.listen(port, () => {
   console.log("Server is serving up in " + port);
 });
+
+
+/*
+  Sử dụng mongoDB Atlas để set up database server(dùng cho production)
+  -Run, create và manage các test case: Jest, Moka,...
+*/
